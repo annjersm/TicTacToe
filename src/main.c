125bec14b38cdaadee70f208845aa2bda306779f
Anders <86270741+annjersm@users.noreply.github.com>
2023-07-02 01:46:27 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
-#include <SDL2/SDL.h>
 #include <stdbool.h>
 
 #include "application.h"
+
+struct global {
+	int tile_size;
+	bool turn;
+	int map[9];
+	int playerX;
+	int playerO;
+	int tie;
+	int w_Width;
+	int w_Height;
+} state;
 
 int tile_size = 200;
 bool turn = true;
@@ -9,15 +19,6 @@ int map[9];
 int playerX = 0;
 int playerO = 0;
 int tie = 0;
-
-SDL_Window* window;
-SDL_Surface* wSurface;
-SDL_Surface* xBmp;
-SDL_Surface* oBmp;
-
-SDL_Point mouse;
-SDL_Point window_size = {800, 800};
-SDL_Rect t1, t2, t3, t4, t5, t6, t7, t8, t9;
 
 int main () {
 	Log();
